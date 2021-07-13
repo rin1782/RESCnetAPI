8aac83d98609200ff3d26e9798812de73382e671
rin1782 <colaerin@yahoo.com>
2021-07-13 06:33:58 -0400

    created user controller and helper methods, session id to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,21 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:user][:email])
 
         if user && user.authenticate(params[:user][:password])
+            session[:id] = user.id
             render json: { status: 201, user: user, logged_in: true }
         else
             render json: { status: 401, message: "Invalid user/password info" }
         end
     end
+
+    def logged_in
+        if logged_in?
+          render json: { status: 201, user: current_user, logged_in: true}
+        else
+          render json: { status: 400, user: {}, logged_in: false}
+        end
+    end
+    
 
     def logout
         reset_session
