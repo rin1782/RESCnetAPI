e621db8608c86c9e60dce1d05507ec46aec5f865
rin1782 <colaerin@yahoo.com>
2021-07-13 06:50:37 -0400

    created user and session create routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
     def create
       user = User.new(user_params)
   
@@ -6,7 +7,7 @@ class UsersController < ApplicationController
         session[:id] = user.id
         render json: { status: 201, user: user, logged_in: true}
       else
-        render json: { status: 500, message: 'Account not created'}
+        render json: { status: 500, message: 'Error: Account not created'}
       end
     end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
+  resources :sessions, only: [:create]
+  resources :users, only: [:create]
+
 end
